553059ba921953b304edf280e357ef51cdedb8e1
haolvh <2569551290@qq.com>
2020-07-28 20:56:09 +0800

    commit config

---
+++ configserver/src/main/java/com/example/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author LvHao
+ * @Description :
+ * @date 2020-07-28 20:54
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}
